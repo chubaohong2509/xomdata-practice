bb9d8d3f5598ed85c0cc51c4af05c80b1b40c622
chubaohong2509 <128879105+chubaohong2509@users.noreply.github.com>
2026-07-23 07:27:12 +0000

    solve: 3 problems (2026-07-23)
    - easy-where-012
    - easy-where-025
    - easy-where-027

---
+++ practice/sql/easy/easy-where-025/solution.sql
@@ -0,0 +1,7 @@
+-- Xom Data · Heavy shipments
+-- Problem: https://xomdata.com/practice/easy-where-025
+-- Solved: 2026-07-23
+
+SELECT waybill_code,weight_kg,shipping_fee from shipments
+where weight_kg > 3
+order by weight_kg desc;
---
+++ practice/sql/easy/easy-where-027/solution.sql
@@ -0,0 +1,7 @@
+-- Xom Data · Top-scoring players
+-- Problem: https://xomdata.com/practice/easy-where-027
+-- Solved: 2026-07-23
+
+select full_name,goals_scored from players
+where goals_scored > 10
+order by goals_scored desc;
